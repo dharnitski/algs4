a5ec47b85de60e665f12b812dbe4590afe073339
Dmitry Harnitski <dharnitski@gmail.com>
2017-01-19 09:41:40 -0500

    work in progress

--- kd-trees/src/PointSET.java
+++ kd-trees/src/PointSET.java
@@ -1,49 +1,67 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
 
 public class PointSET {
-	// construct an empty set of points
-	public PointSET() {
 
-	}
+    SET<Point2D> set;
 
-	// is the set empty?
-	public boolean isEmpty() {
-		return true;
-	}
+    // construct an empty set of points
+    public PointSET() {
+        set = new SET<Point2D>();
+    }
 
-	// number of points in the set
-	public int size() {
-		return 0;
-	}
+    // is the set empty?
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
 
-	// add the point to the set (if it is not already in the set)
-	public void insert(Point2D p) {
+    // number of points in the set
+    public int size() {
+        return set.size();
+    }
 
-	}
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null)
+            throw new NullPointerException();
+        
+        set.add(p);
+    }
 
-	// does the set contain point p?
-	public boolean contains(Point2D p) {
-		return false;
-	}
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null)
+            throw new NullPointerException();
+        
+        return set.contains(p);
+    }
 
-	// draw all points to standard draw
-	public void draw() {
+    // draw all points to standard draw
+    public void draw() {
+        for (Point2D p : set) {
+            p.draw();
+        }
+    }
 
-	}
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null)
+            throw new NullPointerException();
+        
+        return null;
+    }
 
-	// all points that are inside the rectangle
-	public Iterable<Point2D> range(RectHV rect) {
-		return null;
-	}
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new NullPointerException();
+        
+        return null;
+    }
 
-	// a nearest neighbor in the set to point p; null if the set is empty
-	public Point2D nearest(Point2D p) {
-		return null;
-	}
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
 
-	// unit testing of the methods (optional)
-	public static void main(String[] args) {
-
-	}
+    }
 }
